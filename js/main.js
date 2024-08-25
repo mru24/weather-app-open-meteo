bdf168f0282bb4f8f44e9cf850a46eb6f94e8e4e
mru24 <mru24@yahoo.com>
2024-08-25 11:40:20 +0200

    weather code images added

--- js/main.js
+++ js/main.js
@@ -6,10 +6,291 @@ const app = {
     this.response = '';
     console.log("app ready");
     this.document.on('click','.trigger',(e) => {
-      console.log(e.currentTarget);
-
       this.jq(e.currentTarget).find('.hidden').slideToggle();
     })
+
+    this.weatherCode = {
+      "0":{
+        "day":{
+          "description":"Sunny",
+          "image":"http://openweathermap.org/img/wn/01d@2x.png"
+        },
+        "night":{
+          "description":"Clear",
+          "image":"http://openweathermap.org/img/wn/01n@2x.png"
+        }
+      },
+      "1":{
+        "day":{
+          "description":"Mainly Sunny",
+          "image":"http://openweathermap.org/img/wn/01d@2x.png"
+        },
+        "night":{
+          "description":"Mainly Clear",
+          "image":"http://openweathermap.org/img/wn/01n@2x.png"
+        }
+      },
+      "2":{
+        "day":{
+          "description":"Partly Cloudy",
+          "image":"http://openweathermap.org/img/wn/02d@2x.png"
+        },
+        "night":{
+          "description":"Partly Cloudy",
+          "image":"http://openweathermap.org/img/wn/02n@2x.png"
+        }
+      },
+      "3":{
+        "day":{
+          "description":"Cloudy",
+          "image":"http://openweathermap.org/img/wn/03d@2x.png"
+        },
+        "night":{
+          "description":"Cloudy",
+          "image":"http://openweathermap.org/img/wn/03n@2x.png"
+        }
+      },
+      "45":{
+        "day":{
+          "description":"Foggy",
+          "image":"http://openweathermap.org/img/wn/50d@2x.png"
+        },
+        "night":{
+          "description":"Foggy",
+          "image":"http://openweathermap.org/img/wn/50n@2x.png"
+        }
+      },
+      "48":{
+        "day":{
+          "description":"Rime Fog",
+          "image":"http://openweathermap.org/img/wn/50d@2x.png"
+        },
+        "night":{
+          "description":"Rime Fog",
+          "image":"http://openweathermap.org/img/wn/50n@2x.png"
+        }
+      },
+      "51":{
+        "day":{
+          "description":"Light Drizzle",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Light Drizzle",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "53":{
+        "day":{
+          "description":"Drizzle",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Drizzle",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "55":{
+        "day":{
+          "description":"Heavy Drizzle",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Heavy Drizzle",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "56":{
+        "day":{
+          "description":"Light Freezing Drizzle",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Light Freezing Drizzle",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "57":{
+        "day":{
+          "description":"Freezing Drizzle",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Freezing Drizzle",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "61":{
+        "day":{
+          "description":"Light Rain",
+          "image":"http://openweathermap.org/img/wn/10d@2x.png"
+        },
+        "night":{
+          "description":"Light Rain",
+          "image":"http://openweathermap.org/img/wn/10n@2x.png"
+        }
+      },
+      "63":{
+        "day":{
+          "description":"Rain",
+          "image":"http://openweathermap.org/img/wn/10d@2x.png"
+        },
+        "night":{
+          "description":"Rain",
+          "image":"http://openweathermap.org/img/wn/10n@2x.png"
+        }
+      },
+      "65":{
+        "day":{
+          "description":"Heavy Rain",
+          "image":"http://openweathermap.org/img/wn/10d@2x.png"
+        },
+        "night":{
+          "description":"Heavy Rain",
+          "image":"http://openweathermap.org/img/wn/10n@2x.png"
+        }
+      },
+      "66":{
+        "day":{
+          "description":"Light Freezing Rain",
+          "image":"http://openweathermap.org/img/wn/10d@2x.png"
+        },
+        "night":{
+          "description":"Light Freezing Rain",
+          "image":"http://openweathermap.org/img/wn/10n@2x.png"
+        }
+      },
+      "67":{
+        "day":{
+          "description":"Freezing Rain",
+          "image":"http://openweathermap.org/img/wn/10d@2x.png"
+        },
+        "night":{
+          "description":"Freezing Rain",
+          "image":"http://openweathermap.org/img/wn/10n@2x.png"
+        }
+      },
+      "71":{
+        "day":{
+          "description":"Light Snow",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Light Snow",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "73":{
+        "day":{
+          "description":"Snow",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Snow",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "75":{
+        "day":{
+          "description":"Heavy Snow",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Heavy Snow",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "77":{
+        "day":{
+          "description":"Snow Grains",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Snow Grains",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "80":{
+        "day":{
+          "description":"Light Showers",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Light Showers",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "81":{
+        "day":{
+          "description":"Showers",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Showers",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "82":{
+        "day":{
+          "description":"Heavy Showers",
+          "image":"http://openweathermap.org/img/wn/09d@2x.png"
+        },
+        "night":{
+          "description":"Heavy Showers",
+          "image":"http://openweathermap.org/img/wn/09n@2x.png"
+        }
+      },
+      "85":{
+        "day":{
+          "description":"Light Snow Showers",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Light Snow Showers",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "86":{
+        "day":{
+          "description":"Snow Showers",
+          "image":"http://openweathermap.org/img/wn/13d@2x.png"
+        },
+        "night":{
+          "description":"Snow Showers",
+          "image":"http://openweathermap.org/img/wn/13n@2x.png"
+        }
+      },
+      "95":{
+        "day":{
+          "description":"Thunderstorm",
+          "image":"http://openweathermap.org/img/wn/11d@2x.png"
+        },
+        "night":{
+          "description":"Thunderstorm",
+          "image":"http://openweathermap.org/img/wn/11n@2x.png"
+        }
+      },
+      "96":{
+        "day":{
+          "description":"Light Thunderstorms With Hail",
+          "image":"http://openweathermap.org/img/wn/11d@2x.png"
+        },
+        "night":{
+          "description":"Light Thunderstorms With Hail",
+          "image":"http://openweathermap.org/img/wn/11n@2x.png"
+        }
+      },
+      "99":{
+        "day":{
+          "description":"Thunderstorm With Hail",
+          "image":"http://openweathermap.org/img/wn/11d@2x.png"
+        },
+        "night":{
+          "description":"Thunderstorm With Hail",
+          "image":"http://openweathermap.org/img/wn/11n@2x.png"
+        }
+      }
+    }
 
     this.geoSearch = this.jq('#geolocationSearch');
 
@@ -47,7 +328,11 @@ const app = {
     let timezone = coords[2];
 
     if(latitude && longitude && timezone) {
-      this.response = await this.callApi(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,cloud_cover&timezone=${timezone}&forecast_days=16`);
+      this.response = await this.callApi(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant&hourly=temperature_2m,precipitation_probability,precipitation,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,cloud_cover,is_day&timezone=${timezone}&forecast_days=16`);
+
+      // '&hourly=temperature_2m,precipitation,rain,showers,snowfall,snow_depth,weather_code,surface_pressure,visibility,wind_speed_10m,wind_direction_10m,wind_gusts_10m';
+      // '&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,wind_speed_10m_max,wind_gusts_10m_max,wind_direction_10m_dominant';
+
       if(this.response) {
         await this.showWeather(this.response);
         this.jq('#geolocationResults').addClass('d-none');
@@ -60,26 +345,35 @@ const app = {
     html += `<div class="current ${data.current.is_day=1?'day':'night'} trigger">`;
       html += '<div class="row">';
         html += '<div class="col">';
-          html += `<h1>${await this.readWeatherCode(data.current.weather_code)}</h1>`;
+          html += `<h1>${data.current.is_day=1?this.weatherCode[data.current.weather_code]['day']['description']:this.weatherCode[data.current.weather_code]['night']['description']}</h1>`;
         html += '</div>';
       html += '</div>';
 
       html += '<div class="row">';
-        html += '<div class="col">';
-          html += `<h2>${data.current.temperature_2m+data.current_units.temperature_2m}</h2>`;
-          html += `<p>Feels like ${data.current.apparent_temperature+data.current_units.temperature_2m}</p>`;
+        html += '<div class="col" style="max-width:100px;">';
+          html += `<img src="${data.current.is_day=1?this.weatherCode[data.current.weather_code]['day']['image']:this.weatherCode[data.current.weather_code]['night']['image']}" />`;
         html += '</div>';
+
+        html += '<div class="col" style="padding-top:18px;">';
+          html += `<h2>${data.current.temperature_2m+data.current_units.temperature_2m}</h2>`;
+        html += '</div>';
+
         html += '<div class="col">';
-          html += `<p></p>`;
-          html += `<p></p>`;
-          html += `<p class="wind-direction">`;
-            html += `<span class="wind-rose">`;
+          html += '<p class="wind-direction">';
+            html += '<span class="wind-rose">';
               html += `<span class="arrow" style="transform:rotate(${data.current.wind_direction_10m}deg)">&#8595;</span>`;
             html += `</span>`;
           html += `</p>`;
           html += `<p>${data.current.wind_speed_10m} - ${data.current.wind_gusts_10m} ${data.current_units.wind_speed_10m}</p>`;
         html += '</div>';
       html += '</div>';
+
+      html += '<div class="row">';
+        html += '<div class="col">'
+          html += `<p>Feels like ${data.current.apparent_temperature+data.current_units.temperature_2m}</p>`;
+        html += '</div>';
+      html += '</div>';
+
       html += '<div class="hidden">';
         html += '<hr>';
         html += '<p>some text...</p>';
@@ -93,6 +387,7 @@ const app = {
       html += '<div class="col">';
         html += `<p>Today</p><p>${await this.getDayName(day)}</p>`;
       html += '</div>';
+
     html += '</div>';
     html += '<div class="hidden">';
     data.hourly.time.forEach((e,i)=>{
@@ -109,7 +404,11 @@ const app = {
           html += '</div>';
         html += '</div>';
         html += '<div class="forecast day trigger">';
-          html += `<div class="row">${data.hourly.time[i].slice(0,-6)}</div>`;
+          html += '<div class="row">';
+            html += '<div class="col">';
+              html += data.hourly.time[i].slice(0,-6);
+            html += '</div>';
+          html += '</div>';
           html += '<div class="hidden">';
       }
             html += '<div class="row">';
@@ -118,20 +417,21 @@ const app = {
                   html += time[1];
                 html += '</div>';
               html += '</div>';
+
               html += '<div class="col">';
+                html += `<img src="${data.hourly.is_day[i]=1?this.weatherCode[data.hourly.weather_code[i]]['day']['image']:this.weatherCode[data.hourly.weather_code[i]]['night']['image']}" width="40" height="40" style="margin-top:-10px;" />`;
+              html += '</div>';
+
+              html += `<div class="col rain-${data.hourly.rain[i]} showers-${data.hourly.showers[i]}">`;
               if(data.hourly.rain[i]>0 || data.hourly.showers[i]>0) {
                 if(data.hourly.rain[i]>data.hourly.showers[i]) {
                   html += '<img src="img/rain.svg" width="30" />';
-                }
-                if(data.hourly.rain[i]<data.hourly.showers[i]) {
+                } else if(data.hourly.rain[i]<data.hourly.showers[i]) {
                   html += '<img src="img/showers.svg" width="30" />';
+                } else {
+                  html += '<img src="img/rain.svg" width="30" />';
                 }
               }
-              html += '</div>';
-              html += '<div class="col">';
-                if(data.hourly.precipitation_probability[i]>0) {
-                  html += data.hourly.precipitation_probability[i]+'%';
-                }
               html += '</div>';
               html += '<div class="col">';
                 if(data.hourly.precipitation[i]>0) {
@@ -177,111 +477,18 @@ const app = {
     }
     return dayName;
   },
-  async readWeatherCode(code) {
-    let weather = '';
-    switch (code) {
-      case 0:
-        weather = 'Clear sky';
-        break;
-      case 1:
-        weather = 'Mainly clear, partly cloudy, and overcast';
-        break;
-      case 2:
-        weather = 'Mainly clear, partly cloudy, and overcast';
-        break;
-      case 3:
-        weather = 'Mainly clear, partly cloudy, and overcast';
-        break;
-      case 45:
-        weather = 'Fog and depositing rime fog';
-        break;
-      case 48:
-        weather = 'Fog and depositing rime fog';
-        break;
-      case 51:
-        weather = 'Drizzle: Light, moderate, and dense intensity';
-        break;
-      case 53:
-        weather = 'Drizzle: Light, moderate, and dense intensity';
-        break;
-      case 55:
-        weather = 'Drizzle: Light, moderate, and dense intensity';
-        break;
-      case 56:
-        weather = 'Freezing Drizzle: Light and dense intensity';
-        break;
-      case 57:
-        weather = 'Freezing Drizzle: Light and dense intensity';
-        break;
-      case 61:
-        weather = 'Rain: Slight, moderate and heavy intensity';
-        break;
-      case 63:
-        weather = 'Rain: Slight, moderate and heavy intensity';
-        break;
-      case 65:
-        weather = 'Rain: Slight, moderate and heavy intensity';
-        break;
-      case 66:
-        weather = 'Freezing Rain: Light and heavy intensity';
-        break;
-      case 67:
-        weather = 'Freezing Rain: Light and heavy intensity';
-        break;
-      case 71:
-        weather = 'Snow fall: Slight, moderate, and heavy intensity';
-        break;
-      case 73:
-        weather = 'Snow fall: Slight, moderate, and heavy intensity';
-        break;
-      case 75:
-        weather = 'Snow fall: Slight, moderate, and heavy intensity';
-        break;
-      case 77:
-        weather = 'Snow grains';
-        break;
-      case 80:
-        weather = 'Rain showers: Slight, moderate, and violent';
-        break;
-      case 81:
-        weather = 'Rain showers: Slight, moderate, and violent';
-        break;
-      case 82:
-        weather = 'Rain showers: Slight, moderate, and violent';
-        break;
-      case 85:
-        weather = 'Snow showers slight and heavy';
-        break;
-      case 86:
-        weather = 'Snow showers slight and heavy';
-        break;
-      case 95:
-        weather = 'Thunderstorm: Slight or moderate';
-        break;
-      case 96:
-        weather = 'Thunderstorm with slight and heavy hail';
-        break;
-      case 99:
-        weather = 'Thunderstorm with slight and heavy hail';
-        break;
-
-      default:
-        break;
-    }
-    return weather;
-  },
   async callApi(url) {
     const options = {
       method: 'GET'
-  };
-  try {
-      const response = await fetch(url, options);
-      const result = await response.json();
-      console.log("RESULT: ",result);
-      return result;
-  } catch (error) {
-      console.error(error);
-  }
+    };
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        console.log("RESULT: ",result);
+        return result;
+    } catch (error) {
+        console.error(error);
+    }
   }
 }
 app.init();
